2eca35fd49eefb31f1e2af6033c9ade21308ae52
Benjamin Swanepeol <49689582+superbrobenji@users.noreply.github.com>
2024-11-15 17:34:16 +0200

    added throttle func with tests

--- src/promises.ts
+++ src/promises.ts
@@ -1,10 +1,11 @@
+import Asyncrify from "asyncrify";
 interface TimeoutErrorConstants {
-  [erorr: string]: string;
+    [erorr: string]: string;
 }
 
 const TimeoutErrors: TimeoutErrorConstants = {
-  TIMEOUT_ERROR_MESSAGE: "Timeout Error",
-  RESPONSE_ERROR_MESSAGE: "Response Error",
+    TIMEOUT_ERROR_MESSAGE: "Timeout Error",
+    RESPONSE_ERROR_MESSAGE: "Response Error",
 };
 
 /**
@@ -13,7 +14,34 @@ const TimeoutErrors: TimeoutErrorConstants = {
  * @returns {Promise} - The promise that resolves after the time.
  */
 function sleep(time: number = 1000) {
-  return new Promise(resolve => setTimeout(resolve, time));
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+/**
+ * @callback promiseFunction
+ * @return {Promise<T>}
+ */
+
+/**
+ * This method receives an array of functions that returns a promise and a max concurrency
+ * @param {promiseFunction[]} promises - an array of functions that return a promise
+ * @returns {Promise<T[]>} - a promise array of all the promises passed in
+ */
+async function throttle<T>(
+    promises: (() => Promise<T>)[],
+    maxConcurrency: number
+): Promise<T[]> {
+    const returnArray = new Array<Promise<T>>(promises.length);
+    const queue = new Asyncrify(maxConcurrency);
+    for (let i = 0; i < promises.length; i++) {
+        returnArray[i] = new Promise((resolve, reject) => {
+            queue.add(promises[i], (res, err) => {
+                if (err) reject(err);
+                resolve(res);
+            });
+        });
+    }
+    return Promise.all(returnArray);
 }
 
 /**
@@ -26,22 +54,22 @@ function sleep(time: number = 1000) {
  * author: ahn0min - YeongMin Ahn
  */
 function timeout<T>(promise: Promise<T>, time: number = 8000) {
-  return new Promise((resolve, reject) => {
-    const timeout_id = setTimeout(
-      () => reject(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE)),
-      time
-    );
+    return new Promise((resolve, reject) => {
+        const timeout_id = setTimeout(
+            () => reject(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE)),
+            time
+        );
 
-    promise
-      .then(response => {
-        clearTimeout(timeout_id);
-        resolve(response);
-      })
-      .catch(err => {
-        clearTimeout(timeout_id);
-        reject(err);
-      });
-  });
+        promise
+            .then(response => {
+                clearTimeout(timeout_id);
+                resolve(response);
+            })
+            .catch(err => {
+                clearTimeout(timeout_id);
+                reject(err);
+            });
+    });
 }
 
 /**
@@ -51,8 +79,8 @@ function timeout<T>(promise: Promise<T>, time: number = 8000) {
  * @returns {Promise} - The first promise that is resolved.
  */
 async function race<T>(array_of_promises: Promise<T>[]) {
-  const result = await Promise.race(array_of_promises);
-  return result;
+    const result = await Promise.race(array_of_promises);
+    return result;
 }
 
-export { sleep, timeout, race, TimeoutErrors };
+export { sleep, timeout, race, TimeoutErrors, throttle };
