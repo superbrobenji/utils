e8de2f50e1bd0cbe6d29f8151dd7e25f370d10b5
Benjamin Swanepeol <49689582+superbrobenji@users.noreply.github.com>
2024-11-15 17:57:09 +0200

    fixed format

--- __tests__/promises.test.ts
+++ __tests__/promises.test.ts
@@ -1,140 +1,140 @@
 const { sleep, timeout, race, TimeoutErrors, throttle } = require("@/promises");
 
 describe("sleep", () => {
-    test("sleeps for half a second passing 500", async () => {
-        const start: Date = new Date();
-        await sleep(500);
-        const end: Date = new Date();
-        const diff: number = end.getTime() - start.getTime();
-        expect(diff).toBeGreaterThanOrEqual(499);
-    });
+  test("sleeps for half a second passing 500", async () => {
+    const start: Date = new Date();
+    await sleep(500);
+    const end: Date = new Date();
+    const diff: number = end.getTime() - start.getTime();
+    expect(diff).toBeGreaterThanOrEqual(499);
+  });
 
-    test("sleeps for 0.1 seconds passing 100", async () => {
-        const start: Date = new Date();
-        await sleep(100);
-        const end: Date = new Date();
-        const diff: number = end.getTime() - start.getTime();
-        expect(diff).toBeGreaterThanOrEqual(99);
-    });
+  test("sleeps for 0.1 seconds passing 100", async () => {
+    const start: Date = new Date();
+    await sleep(100);
+    const end: Date = new Date();
+    const diff: number = end.getTime() - start.getTime();
+    expect(diff).toBeGreaterThanOrEqual(99);
+  });
 
-    test("sleeps for 1 second without params", async () => {
-        const start: Date = new Date();
-        await sleep();
-        const end: Date = new Date();
-        const diff: number = end.getTime() - start.getTime();
-        expect(diff).toBeGreaterThanOrEqual(999);
-    });
+  test("sleeps for 1 second without params", async () => {
+    const start: Date = new Date();
+    await sleep();
+    const end: Date = new Date();
+    const diff: number = end.getTime() - start.getTime();
+    expect(diff).toBeGreaterThanOrEqual(999);
+  });
 });
 
 function mockResolvedPromise(value: any, time: number) {
-    return new Promise(resolve => setTimeout(() => resolve(value), time));
+  return new Promise(resolve => setTimeout(() => resolve(value), time));
 }
 
 function mockRejectedPromise(error: any, time: number) {
-    return new Promise((_, reject) =>
-        setTimeout(() => reject(new Error(error)), time)
-    );
+  return new Promise((_, reject) =>
+    setTimeout(() => reject(new Error(error)), time)
+  );
 }
 
 describe("Throttle", () => {
-    test("In case of only resolve", async () => {
-        const data = "resolved data";
-        const success = await throttle([
-            () => mockResolvedPromise(data, 100),
-            () => mockResolvedPromise(data, 200),
-            () => mockResolvedPromise(data, 300),
-        ]);
-        expect(success).toEqual([data, data, data]);
-    });
-    test("In case of one rejected", async () => {
-        const data = "resolved data";
-        await expect(() =>
-            throttle([
-                () => mockResolvedPromise(data, 100),
-                () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
-                () => mockResolvedPromise(data, 300),
-            ])
-        ).rejects.toThrow(TimeoutErrors.RESPONSE_ERROR_MESSAGE);
-    });
-    test("In case of all rejected", async () => {
-        const data = "resolved data";
-        await expect(() =>
-            throttle([
-                () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
-                () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
-                () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
-            ])
-        ).rejects.toThrow(TimeoutErrors.RESPONSE_ERROR_MESSAGE);
-    });
+  test("In case of only resolve", async () => {
+    const data = "resolved data";
+    const success = await throttle([
+      () => mockResolvedPromise(data, 100),
+      () => mockResolvedPromise(data, 200),
+      () => mockResolvedPromise(data, 300),
+    ]);
+    expect(success).toEqual([data, data, data]);
+  });
+  test("In case of one rejected", async () => {
+    const data = "resolved data";
+    await expect(() =>
+      throttle([
+        () => mockResolvedPromise(data, 100),
+        () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
+        () => mockResolvedPromise(data, 300),
+      ])
+    ).rejects.toThrow(TimeoutErrors.RESPONSE_ERROR_MESSAGE);
+  });
+  test("In case of all rejected", async () => {
+    const data = "resolved data";
+    await expect(() =>
+      throttle([
+        () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
+        () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
+        () => mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 200),
+      ])
+    ).rejects.toThrow(TimeoutErrors.RESPONSE_ERROR_MESSAGE);
+  });
 });
 
 describe("timeout", () => {
-    const data = "resolved data";
+  const data = "resolved data";
 
-    test("In case of only resolve", async () => {
-        const success = await timeout(mockResolvedPromise(data, 100), 300);
-        expect(success).toBe(data);
-    });
+  test("In case of only resolve", async () => {
+    const success = await timeout(mockResolvedPromise(data, 100), 300);
+    expect(success).toBe(data);
+  });
 
-    test("In case of only rejected", async () => {
-        await expect(() =>
-            timeout(
-                mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 100),
-                300
-            )
-        ).rejects.toThrow(new Error(TimeoutErrors.RESPONSE_ERROR_MESSAGE));
-    });
+  test("In case of only rejected", async () => {
+    await expect(() =>
+      timeout(
+        mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 100),
+        300
+      )
+    ).rejects.toThrow(new Error(TimeoutErrors.RESPONSE_ERROR_MESSAGE));
+  });
 
-    test("In case of timeout and resolve", async () => {
-        await expect(() =>
-            timeout(mockResolvedPromise(data, 300), 100)
-        ).rejects.toThrow(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE));
-    });
+  test("In case of timeout and resolve", async () => {
+    await expect(() =>
+      timeout(mockResolvedPromise(data, 300), 100)
+    ).rejects.toThrow(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE));
+  });
 
-    test("In case of timeout and rejected", async () => {
-        await expect(() =>
-            timeout(
-                mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 300),
-                100
-            )
-        ).rejects.toThrow(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE));
-    });
+  test("In case of timeout and rejected", async () => {
+    await expect(() =>
+      timeout(
+        mockRejectedPromise(TimeoutErrors.RESPONSE_ERROR_MESSAGE, 300),
+        100
+      )
+    ).rejects.toThrow(new Error(TimeoutErrors.TIMEOUT_ERROR_MESSAGE));
+  });
 });
 
 describe("race", () => {
-    const sleepForNTime = (time: number) =>
-        new Promise(async resolve => {
-            await sleep(time);
-            return resolve(time);
-        });
-
-    test("should return the first promise as faster", async () => {
-        const first = sleepForNTime(500);
-        const second = sleepForNTime(1000);
-        const third = sleepForNTime(2000);
-
-        const race_winner = await race([first, second, third]);
-
-        expect(race_winner).toBe(500);
+  const sleepForNTime = (time: number) =>
+    new Promise(async resolve => {
+      await sleep(time);
+      return resolve(time);
     });
 
-    test("should return the second promise as faster", async () => {
-        const first = sleepForNTime(1000);
-        const second = sleepForNTime(500);
-        const third = sleepForNTime(2000);
+  test("should return the first promise as faster", async () => {
+    const first = sleepForNTime(500);
+    const second = sleepForNTime(1000);
+    const third = sleepForNTime(2000);
 
-        const race_winner = await race([first, second, third]);
+    const race_winner = await race([first, second, third]);
 
-        expect(race_winner).toBe(500);
-    });
+    expect(race_winner).toBe(500);
+  });
 
-    test("should return the third promise as faster", async () => {
-        const first = sleepForNTime(2000);
-        const second = sleepForNTime(1000);
-        const third = sleepForNTime(500);
+  test("should return the second promise as faster", async () => {
+    const first = sleepForNTime(1000);
+    const second = sleepForNTime(500);
+    const third = sleepForNTime(2000);
 
-        const race_winner = await race([first, second, third]);
+    const race_winner = await race([first, second, third]);
 
-        expect(race_winner).toBe(500);
-    });
+    expect(race_winner).toBe(500);
+  });
+
+  test("should return the third promise as faster", async () => {
+    const first = sleepForNTime(2000);
+    const second = sleepForNTime(1000);
+    const third = sleepForNTime(500);
+
+    const race_winner = await race([first, second, third]);
+
+    expect(race_winner).toBe(500);
+  });
 });
